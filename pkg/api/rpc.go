0e55aa145a9308c66ff47c2050d7bcc8b4ea4372
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-03-07 15:53:10 +0000

    Use new depot.cli.v1.BuildService

--- pkg/api/rpc.go
+++ pkg/api/rpc.go
@@ -5,15 +5,16 @@ import (
 	"os"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/depot/cli/pkg/proto/depot/cli/v1/cliv1connect"
 	"github.com/depot/cli/pkg/proto/depot/cli/v1beta1/cliv1beta1connect"
 )
 
-func NewBuildClient() cliv1beta1connect.BuildServiceClient {
+func NewBuildClient() cliv1connect.BuildServiceClient {
 	baseURL := os.Getenv("DEPOT_API_URL")
 	if baseURL == "" {
 		baseURL = "https://api.depot.dev"
 	}
-	return cliv1beta1connect.NewBuildServiceClient(http.DefaultClient, baseURL, connect.WithGRPC(), WithUserAgent())
+	return cliv1connect.NewBuildServiceClient(http.DefaultClient, baseURL, connect.WithGRPC(), WithUserAgent())
 }
 
 func NewLoginClient() cliv1beta1connect.LoginServiceClient {
