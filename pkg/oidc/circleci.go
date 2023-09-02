c0bacb0097b79a889d2d139a3d3fb91affeb3bec
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-09-02 13:10:42 +0100

    Add support for acquiring CircleCI OIDC token

---
+++ pkg/oidc/circleci.go
@@ -0,0 +1,18 @@
+package oidc
+
+import (
+	"context"
+	"os"
+)
+
+type CircleCIOIDCProvider struct {
+}
+
+func NewCircleCIOIDCProvider() *CircleCIOIDCProvider {
+	return &CircleCIOIDCProvider{}
+}
+
+func (p *CircleCIOIDCProvider) RetrieveToken(ctx context.Context) (string, error) {
+	token := os.Getenv("CIRCLE_OIDC_TOKEN_V2")
+	return token, nil
+}
