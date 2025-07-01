f07b00b1a9d94bc5203b5961aba63668387ba18a
Alexis Bouchez <209880115+alexisbcz@users.noreply.github.com>
2025-07-01 21:29:01 +0200

    feat: fetch organizations from api

--- pkg/api/rpc.go
+++ pkg/api/rpc.go
@@ -1,43 +1,70 @@
 package api
 
 import (
+	"crypto/tls"
+	"net"
 	"net/http"
 	"os"
+	"strings"
 
 	"buf.build/gen/go/depot/api/connectrpc/go/depot/core/v1/corev1connect"
 	"connectrpc.com/connect"
 	"github.com/depot/cli/pkg/proto/depot/agent/v1/agentv1connect"
 	"github.com/depot/cli/pkg/proto/depot/cli/v1/cliv1connect"
 	"github.com/depot/cli/pkg/proto/depot/cli/v1beta1/cliv1beta1connect"
+	cliCorev1connect "github.com/depot/cli/pkg/proto/depot/core/v1/corev1connect"
+	"golang.org/x/net/http2"
 )
 
 func NewBuildClient() cliv1connect.BuildServiceClient {
-	return cliv1connect.NewBuildServiceClient(http.DefaultClient, getBaseURL(), WithUserAgent())
+	return cliv1connect.NewBuildServiceClient(getHTTPClient(getBaseURL()), getBaseURL(), WithUserAgent())
 }
 
 func NewLoginClient() cliv1beta1connect.LoginServiceClient {
-	return cliv1beta1connect.NewLoginServiceClient(http.DefaultClient, getBaseURL(), WithUserAgent())
+	return cliv1beta1connect.NewLoginServiceClient(getHTTPClient(getBaseURL()), getBaseURL(), WithUserAgent())
 }
 
 func NewProjectsClient() cliv1beta1connect.ProjectsServiceClient {
-	return cliv1beta1connect.NewProjectsServiceClient(http.DefaultClient, getBaseURL(), WithUserAgent())
+	return cliv1beta1connect.NewProjectsServiceClient(getHTTPClient(getBaseURL()), getBaseURL(), WithUserAgent())
+}
+
+func NewOrganizationsClient() cliCorev1connect.OrganizationServiceClient {
+	return cliCorev1connect.NewOrganizationServiceClient(getHTTPClient(getBaseURL()), getBaseURL(), WithUserAgent())
 }
 
 func NewSDKProjectsClient() corev1connect.ProjectServiceClient {
-	return corev1connect.NewProjectServiceClient(http.DefaultClient, getBaseURL(), WithUserAgent())
+	return corev1connect.NewProjectServiceClient(getHTTPClient(getBaseURL()), getBaseURL(), WithUserAgent())
 }
 
 func NewPushClient() cliv1connect.PushServiceClient {
-	return cliv1connect.NewPushServiceClient(http.DefaultClient, getBaseURL(), WithUserAgent())
+	return cliv1connect.NewPushServiceClient(getHTTPClient(getBaseURL()), getBaseURL(), WithUserAgent())
 }
 
 func NewClaudeClient() agentv1connect.ClaudeServiceClient {
-	return agentv1connect.NewClaudeServiceClient(http.DefaultClient, getBaseURL(), WithUserAgent())
+	return agentv1connect.NewClaudeServiceClient(getHTTPClient(getBaseURL()), getBaseURL(), WithUserAgent())
 }
 
 func WithAuthentication[T any](req *connect.Request[T], token string) *connect.Request[T] {
 	req.Header().Add("Authorization", "Bearer "+token)
 	return req
+}
+
+// getHTTPClient returns an HTTP client configured for the given base URL.
+// If the URL uses HTTP (not HTTPS), it configures h2c support for HTTP/2 over cleartext.
+func getHTTPClient(baseURL string) *http.Client {
+	if strings.HasPrefix(baseURL, "http://") {
+		// Configure h2c (HTTP/2 over cleartext) for non-TLS connections
+		return &http.Client{
+			Transport: &http2.Transport{
+				AllowHTTP: true,
+				DialTLS: func(network, addr string, _ *tls.Config) (net.Conn, error) {
+					return net.Dial(network, addr)
+				},
+			},
+		}
+	}
+	// Use default client for HTTPS connections
+	return http.DefaultClient
 }
 
 func getBaseURL() string {
--- pkg/helpers/organization.go
+++ pkg/helpers/organization.go
@@ -1,10 +1,15 @@
 package helpers
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
+	"connectrpc.com/connect"
 	"github.com/charmbracelet/huh"
+	"github.com/depot/cli/pkg/api"
+	"github.com/depot/cli/pkg/config"
+	corev1 "github.com/depot/cli/pkg/proto/depot/core/v1"
 )
 
 type Organization struct {
@@ -13,27 +18,31 @@ type Organization struct {
 }
 
 func RetrieveOrganizations() ([]*Organization, error) {
-	// Fake organizations
-	return []*Organization{
-		{
-			OrgId: "1",
-			Name:  "Organization 1",
-		},
-		{
-			OrgId: "2",
-			Name:  "Organization 2",
-		},
-	}, nil
+	client := api.NewOrganizationsClient()
+	req := corev1.ListOrganizationRequest{}
+	resp, err := client.ListOrganizations(
+		context.Background(),
+		api.WithAuthentication(connect.NewRequest(&req), config.GetApiToken()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	organizations := []*Organization{}
+	for _, org := range resp.Msg.Organizations {
+		organizations = append(organizations, &Organization{
+			OrgId: org.OrgId,
+			Name:  org.Name,
+		})
+	}
+
+	return organizations, nil
 }
 
 func SelectOrganization() (*Organization, error) {
 	organizations, err := RetrieveOrganizations()
 	if err != nil {
 		return nil, err
-	}
-
-	if len(organizations) == 1 {
-		return organizations[0], nil
 	}
 
 	huhOptions := make([]huh.Option[*Organization], len(organizations))
