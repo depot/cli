eaf1c2e9e9266508da47208916c5ec2289a9b1f3
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2026-05-13 12:05:32 -0400

    Implement convenience command `depot login-docker`

---
+++ pkg/cmd/docker/login.go
@@ -0,0 +1,71 @@
+package docker
+
+import (
+	"fmt"
+
+	"github.com/depot/cli/pkg/config"
+	"github.com/depot/cli/pkg/dockerclient"
+	configtypes "github.com/docker/cli/cli/config/types"
+	registrytypes "github.com/docker/docker/api/types/registry"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdLoginDocker() *cobra.Command {
+	var (
+		org   string
+		token string
+	)
+
+	cmd := &cobra.Command{
+		Use:   "login-docker",
+		Short: "Log in Docker to the Depot registry with your user API token",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if token == "" {
+				token = config.GetApiToken()
+			}
+			if token == "" {
+				return fmt.Errorf("missing API token, please run `depot login`")
+			}
+
+			if org == "" {
+				org = config.GetCurrentOrganization()
+			}
+
+			dockerCli, err := dockerclient.NewDockerCLI()
+			if err != nil {
+				return err
+			}
+
+			serverAddress := org + ".registry.depot.dev"
+			client := dockerCli.Client()
+			resp, err := client.RegistryLogin(cmd.Context(), registrytypes.AuthConfig{
+				Username:      "x-token",
+				Password:      token,
+				ServerAddress: serverAddress,
+			})
+			if err != nil {
+				return err
+			}
+
+			cfg := dockerCli.ConfigFile()
+			creds := cfg.GetCredentialsStore(serverAddress)
+			err = creds.Store(configtypes.AuthConfig{
+				Username:      "x-token",
+				Password:      token,
+				ServerAddress: serverAddress,
+			})
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Successfully stored Docker credentials for" + serverAddress)
+			return nil
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&org, "org", "", "Depot organization ID")
+	flags.StringVar(&token, "token", "", "Depot token")
+
+	return cmd
+}
